87f21222bcb9cf0d34289c7b60c97ed079d93faa
JimmyCN1 <chibueze.e.njoku@gmail.com>
2019-07-02 17:33:11 +1000

    implemented US telephone number validator

--- telephoneNumberValidator.js
+++ telephoneNumberValidator.js
@@ -1,5 +1,34 @@
+// this algortihm puts a string through a number of regex validations
+// to determine whether or not the string passed is a valid US number
+
 function telephoneCheck(str) {
-  return true;
+  // must have 10 or 11 digits
+  let isValidNumber = true;
+  // check for invalid characters
+  if (str.match(/[^0-9\-() ]+/)) {
+    isValidNumber = false;
+    // check for incomplete parenthesis
+  } else if (
+    (str.match(/\({1}/) && !str.match(/\){1}/)) ||
+    (!str.match(/\({1}/) && str.match(/\){1}/))
+  ) {
+    isValidNumber = false;
+    // check if the string starts with a hyphen
+  } else if (str.match(/^[\-]/)) {
+    isValidNumber = false;
+    // check if the string both starts and ends with a hyphen
+  } else if (str.match(/^\(/) && str.match(/\)$/)) {
+    isValidNumber = false;
+  }
+  const onlyDigits = str.replace(/[^0-9]/g, "").trim();
+  // check if there are too many or too few digits
+  if (onlyDigits.length < 10 || onlyDigits.length > 11) {
+    isValidNumber = false;
+    // check if string doesnt start with a one when the number of digits does not equal to 10
+  } else if (onlyDigits.match(/^[^1]/) && onlyDigits.length !== 10) {
+    isValidNumber = false;
+  }
+  return isValidNumber;
 }
 
 // Tests
